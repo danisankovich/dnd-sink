bd1594d3ddd1429a8e41fa0015c96f23f8181782
Michael Sankovich <misankovich@vassar.edu>
2020-06-29 13:33:59 -0400

    fix if statement

--- index.js
+++ index.js
@@ -41,175 +41,178 @@ function timeoutChecker(msg, serverQueue, voiceChannel) {
 }
 
 client.on('message', msg => {
-  const serverQueue = queue.get(msg.guild.id);
-  state[msg.guild.id] = state[msg.guild.id] || {};
+  if (msg.guild.id) {
+    const serverQueue = queue.get(msg.guild.id);
+    state[msg.guild.id] = state[msg.guild.id] || {};
 
-  if (msg.content.startsWith('!play')) {
-    // }, 60000 * 60); // disconnect on an hour
-      return getMusic(msg, serverQueue, queue, state, client).then((voiceChannel) => {
-        timeoutChecker(msg, serverQueue, voiceChannel)
+    if (msg.content.startsWith('!play')) {
+      // }, 60000 * 60); // disconnect on an hour
+        return getMusic(msg, serverQueue, queue, state, client).then((voiceChannel) => {
+          timeoutChecker(msg, serverQueue, voiceChannel)
+        }).catch(err => {
+          console.error(`${new Date()} ${err}`);
+        });
+    }
+    if (msg.content.startsWith('!start')) {
+
+        return playPlaylist(msg, serverQueue, queue, state, client).then(voiceChannel => {
+          timeoutChecker(msg, serverQueue, voiceChannel)
+        });
+    }
+    if (msg.content.startsWith('!addsong')) {
+        return addSongToPlaylist(msg);
+    }
+    if (msg.content.startsWith('!removesong')) {
+        return removeSongFromPlaylist(msg);
+    }
+    if (msg.content.startsWith('!newplaylist')) {
+        return createPlaylist(msg);
+    }
+    if (msg.content.startsWith('!deleteplaylist')) {
+        return deletePlaylist(msg);
+    }
+    if (msg.content.startsWith('!showplaylistsongs')) {
+        return displayPlaylistSongs(msg);
+    }
+    if (msg.content.startsWith('!showplaylists')) {
+        return displayPlaylists(msg);
+    }
+    if (msg.content.startsWith('!next')) {
+      if (!serverQueue) {
+        return msg.channel.send(`Music must be in the queue to use this command`);
+      }
+      timeoutChecker(msg, serverQueue)
+      next(serverQueue, msg.guild, queue, state);
+    }
+    if (msg.content.startsWith('!restart')) {
+      if (!serverQueue) {
+        return msg.channel.send(`Music must be in the queue to use this command`);
+      }
+      timeoutChecker(msg, serverQueue)
+      play(msg.guild, serverQueue.songs[0], queue, state);
+      return msg.channel.send(`Restarting current song`);
+    }
+    if (msg.content.startsWith('!stop')) {
+      if (!serverQueue) {
+        return msg.channel.send(`Music must be in the queue to use this command`);
+      }
+      state[msg.guild.id].timeoutHandle = state[msg.guild.id].timeoutHandle;
+      state[msg.guild.id].voiceChannel = serverQueue.voiceChannel;
+      clearTimeout(state[msg.guild.id].timeoutHandle);
+      state[msg.guild.id].timeoutHandle = undefined;
+      stop(serverQueue);
+      return msg.channel.send(`Stopping music stream`);
+    }
+    if (msg.content.startsWith('!loopsong')) {
+      if (!serverQueue) {
+        return msg.channel.send(`Music must be in the queue to use this command`);
+      }
+      state[msg.guild.id] = state[msg.guild.id] || {};
+      state[msg.guild.id].loopSong = !state[msg.guild.id].loopSong;
+
+      timeoutChecker(msg, serverQueue)
+      return msg.channel.send(`Looping is now turned ${state[msg.guild.id].loopSong ? 'On' : 'Off'} for the current song`);
+    }
+    if (msg.content.startsWith('!loop')) {
+      if (!serverQueue) {
+        return msg.channel.send(`Music must be in the queue to use this command`);
+      }
+      state[msg.guild.id] = state[msg.guild.id] || {};
+      state[msg.guild.id].loop = !state[msg.guild.id].loop;
+
+      timeoutChecker(msg, serverQueue)
+      return msg.channel.send(`Looping is now turned ${state[msg.guild.id].loop ? 'On' : 'Off'} for the queue`);
+    }
+    if (msg.content.startsWith('!queue')) {
+      if (!serverQueue) {
+        return msg.channel.send(`Music must be in the queue to use this command`);
+      }
+
+      timeoutChecker(msg, serverQueue)
+
+      const songString = serverQueue.songs.map((song, i) => `${i + 1}) ${song.title}`).join('\n')
+      return msg.channel.send(`\`\`\`| Queue:\n${songString}\`\`\``);
+    }
+    if (msg.content.startsWith('!remove')) {
+      if (!serverQueue) {
+        return msg.channel.send(`Music must be in the queue to use this command`);
+      }
+
+      timeoutChecker(msg, serverQueue)
+
+      return remove(msg, serverQueue, queue, state)
+    }
+    if (msg.content.startsWith('!clear')) {
+      if (!serverQueue) {
+        return msg.channel.send(`Music must be in the queue to use this command`);
+      }
+      serverQueue.songs = [];
+
+      timeoutChecker(msg, serverQueue)
+
+      return msg.channel.send(`Queue has been cleared`);
+    }
+    if (msg.content.startsWith('!roll')) {
+      return rollDice(msg);
+    }
+    if (msg.content.startsWith('!stats')) {
+      const type = msg.content.substr(msg.content.indexOf(' ')+1).toLowerCase();
+      let string;
+      if (type === '3d6') {
+        string = rollStats3d6();
+      } else if (type === '4d6') {
+        string = rollStats4d6DropLowest();
+      } else if (type === '4d6*') {
+        string = rollStats4d6DropLowestForgiving();
+      } else if (type === 'd20') {
+        string = rollStatsd20();
+      } else {
+        string = 'Stat type must be "3d6", "4d6", "4d6*", or "d20"';
+      }
+      return msg.reply(`\`\`\`${string}\`\`\``);
+    }
+    if (msg.content.startsWith('!adv') || msg.content.startsWith('!disadv')) {
+      return rollDice(msg, true);
+    }
+    if (msg.content.startsWith('!spell')) {
+      const spellName = msg.content.substr(msg.content.indexOf(' ')+1)
+      return msg.channel.send(`${findSpellByName(spellName)}`);
+    }
+    if (msg.content.startsWith('!condition')) {
+      return msg.channel.send(`${findConditionByName(msg)}`);
+    }
+    if (msg.content.startsWith('!generate')) {
+      return msg.channel.send(`${generateNPC(msg)}`);
+    }
+    if (msg.content.startsWith('!barbarian')) {
+      return classLookup('barbarian', msg);
+    }
+    if (msg.content.startsWith('!bard')) {
+      return classLookup('bard', msg);
+    }
+    if (msg.content.startsWith('!cleric')) {
+      return classLookup('cleric', msg);
+    }
+    if (msg.content.startsWith('!blood hunter')) {
+      return classLookup('blood-hunter', msg);
+    }
+    if (msg.content.startsWith('!help')) {
+      return msg.channel.send(`List of commands can be found here: https://danisankovich.github.io/dnd-sink/`);
+    }
+    if (msg.content.startsWith('!feedback')) {
+      const content = msg.content.substr(msg.content.indexOf(' ')+1);
+      if (content === '!feedback') {
+        return msg.reply('Must provide feedback');
+      }
+      feedback(content).then(e => {
+        return msg.reply('Feedback sent. Thank you for your input.');
       }).catch(err => {
-        console.error(`${new Date()} ${err}`);
-      });
+        console.err(`${new Date()} ${err}`)
+      })
+    }
+    if (msg.content.startsWith('!test')) {
+      console.log(msg, client.user.id)
+    }
   }
-  if (msg.content.startsWith('!start')) {
 
-      return playPlaylist(msg, serverQueue, queue, state, client).then(voiceChannel => {
-        timeoutChecker(msg, serverQueue, voiceChannel)
-      });
-  }
-  if (msg.content.startsWith('!addsong')) {
-      return addSongToPlaylist(msg);
-  }
-  if (msg.content.startsWith('!removesong')) {
-      return removeSongFromPlaylist(msg);
-  }
-  if (msg.content.startsWith('!newplaylist')) {
-      return createPlaylist(msg);
-  }
-  if (msg.content.startsWith('!deleteplaylist')) {
-      return deletePlaylist(msg);
-  }
-  if (msg.content.startsWith('!showplaylistsongs')) {
-      return displayPlaylistSongs(msg);
-  }
-  if (msg.content.startsWith('!showplaylists')) {
-      return displayPlaylists(msg);
-  }
-  if (msg.content.startsWith('!next')) {
-    if (!serverQueue) {
-      return msg.channel.send(`Music must be in the queue to use this command`);
-    }
-    timeoutChecker(msg, serverQueue)
-    next(serverQueue, msg.guild, queue, state);
-  }
-  if (msg.content.startsWith('!restart')) {
-    if (!serverQueue) {
-      return msg.channel.send(`Music must be in the queue to use this command`);
-    }
-    timeoutChecker(msg, serverQueue)
-    play(msg.guild, serverQueue.songs[0], queue, state);
-    return msg.channel.send(`Restarting current song`);
-  }
-  if (msg.content.startsWith('!stop')) {
-    if (!serverQueue) {
-      return msg.channel.send(`Music must be in the queue to use this command`);
-    }
-    state[msg.guild.id].timeoutHandle = state[msg.guild.id].timeoutHandle;
-    state[msg.guild.id].voiceChannel = serverQueue.voiceChannel;
-    clearTimeout(state[msg.guild.id].timeoutHandle);
-    state[msg.guild.id].timeoutHandle = undefined;
-    stop(serverQueue);
-    return msg.channel.send(`Stopping music stream`);
-  }
-  if (msg.content.startsWith('!loopsong')) {
-    if (!serverQueue) {
-      return msg.channel.send(`Music must be in the queue to use this command`);
-    }
-    state[msg.guild.id] = state[msg.guild.id] || {};
-    state[msg.guild.id].loopSong = !state[msg.guild.id].loopSong;
-
-    timeoutChecker(msg, serverQueue)
-    return msg.channel.send(`Looping is now turned ${state[msg.guild.id].loopSong ? 'On' : 'Off'} for the current song`);
-  }
-  if (msg.content.startsWith('!loop')) {
-    if (!serverQueue) {
-      return msg.channel.send(`Music must be in the queue to use this command`);
-    }
-    state[msg.guild.id] = state[msg.guild.id] || {};
-    state[msg.guild.id].loop = !state[msg.guild.id].loop;
-
-    timeoutChecker(msg, serverQueue)
-    return msg.channel.send(`Looping is now turned ${state[msg.guild.id].loop ? 'On' : 'Off'} for the queue`);
-  }
-  if (msg.content.startsWith('!queue')) {
-    if (!serverQueue) {
-      return msg.channel.send(`Music must be in the queue to use this command`);
-    }
-
-    timeoutChecker(msg, serverQueue)
-
-    const songString = serverQueue.songs.map((song, i) => `${i + 1}) ${song.title}`).join('\n')
-    return msg.channel.send(`\`\`\`| Queue:\n${songString}\`\`\``);
-  }
-  if (msg.content.startsWith('!remove')) {
-    if (!serverQueue) {
-      return msg.channel.send(`Music must be in the queue to use this command`);
-    }
-
-    timeoutChecker(msg, serverQueue)
-
-    return remove(msg, serverQueue, queue, state)
-  }
-  if (msg.content.startsWith('!clear')) {
-    if (!serverQueue) {
-      return msg.channel.send(`Music must be in the queue to use this command`);
-    }
-    serverQueue.songs = [];
-
-    timeoutChecker(msg, serverQueue)
-
-    return msg.channel.send(`Queue has been cleared`);
-  }
-  if (msg.content.startsWith('!roll')) {
-    return rollDice(msg);
-  }
-  if (msg.content.startsWith('!stats')) {
-    const type = msg.content.substr(msg.content.indexOf(' ')+1).toLowerCase();
-    let string;
-    if (type === '3d6') {
-      string = rollStats3d6();
-    } else if (type === '4d6') {
-      string = rollStats4d6DropLowest();
-    } else if (type === '4d6*') {
-      string = rollStats4d6DropLowestForgiving();
-    } else if (type === 'd20') {
-      string = rollStatsd20();
-    } else {
-      string = 'Stat type must be "3d6", "4d6", "4d6*", or "d20"';
-    }
-    return msg.reply(`\`\`\`${string}\`\`\``);
-  }
-  if (msg.content.startsWith('!adv') || msg.content.startsWith('!disadv')) {
-    return rollDice(msg, true);
-  }
-  if (msg.content.startsWith('!spell')) {
-    const spellName = msg.content.substr(msg.content.indexOf(' ')+1)
-    return msg.channel.send(`${findSpellByName(spellName)}`);
-  }
-  if (msg.content.startsWith('!condition')) {
-    return msg.channel.send(`${findConditionByName(msg)}`);
-  }
-  if (msg.content.startsWith('!generate')) {
-    return msg.channel.send(`${generateNPC(msg)}`);
-  }
-  if (msg.content.startsWith('!barbarian')) {
-    return classLookup('barbarian', msg);
-  }
-  if (msg.content.startsWith('!bard')) {
-    return classLookup('bard', msg);
-  }
-  if (msg.content.startsWith('!cleric')) {
-    return classLookup('cleric', msg);
-  }
-  if (msg.content.startsWith('!blood hunter')) {
-    return classLookup('blood-hunter', msg);
-  }
-  if (msg.content.startsWith('!help')) {
-    return msg.channel.send(`List of commands can be found here: https://danisankovich.github.io/dnd-sink/`);
-  }
-  if (msg.content.startsWith('!feedback')) {
-    const content = msg.content.substr(msg.content.indexOf(' ')+1);
-    if (content === '!feedback') {
-      return msg.reply('Must provide feedback');
-    }
-    feedback(content).then(e => {
-      return msg.reply('Feedback sent. Thank you for your input.');
-    }).catch(err => {
-      console.err(`${new Date()} ${err}`)
-    })
-  }
-  if (msg.content.startsWith('!test')) {
-    console.log(msg, client.user.id)
-  }
 });
